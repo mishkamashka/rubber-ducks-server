29a21164f4cc9092996098bd7c886d1e32e7cd05
Mariia Anisimova <just-another-log-in@yandex.ru>
2018-11-02 22:42:27 +0300

    add UserService

--- src/main/java/se/ifmo/ru/service/DuckService.java
+++ src/main/java/se/ifmo/ru/service/DuckService.java
@@ -1,4 +1,26 @@
 package se.ifmo.ru.service;
 
+import se.ifmo.ru.dao.DuckDao;
+import se.ifmo.ru.model.Duck;
+
 public class DuckService {
+
+    private DuckDao duckDao = new DuckDao();
+
+    public Duck getDuckById(int id) {
+        return duckDao.getDuckById(id);
+    }
+
+    public void saveDuck(Duck duck) {
+        duckDao.saveDuck(duck);
+    }
+
+    public void deleteDuck(Duck duck) {
+        duckDao.deleteDuck(duck);
+    }
+
+    public void updateDuck(Duck duck) {
+        duckDao.updateDuck(duck);
+    }
+
 }
