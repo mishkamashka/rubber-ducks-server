00965502a0d159d76346d66a92edc35a813b2544
Mariia Anisimova <just-another-log-in@yandex.ru>
2018-11-14 12:41:07 +0300

    add methods to RequestService

--- src/main/java/se/ifmo/ru/dao/RequestDao.java
+++ src/main/java/se/ifmo/ru/dao/RequestDao.java
@@ -2,23 +2,31 @@ package se.ifmo.ru.dao;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import se.ifmo.ru.model.Duck;
 import se.ifmo.ru.model.Request;
 import se.ifmo.ru.util.HibernateSessionFactoryUtil;
 
 import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class RequestDao {
 
     private Session session;
     private Transaction transaction;
-    private Request request;
 
     public Request getById(long id) {
         session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        request = session.get(Request.class, id);
+        Request request = session.get(Request.class, id);
         session.close();
         return request;
+    }
+
+    public Request getByIdWithUserAndDuck(long id) {
+        session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Query query = session.createQuery("from Request r inner join r.user u inner join r.duck d where u.id = r.user.id and d.id = r.duck.id and r.id = " + id);
+        return this.handleJoinResult(query).get(0);
     }
 
     public void save(Request request) {
@@ -52,8 +60,35 @@ public class RequestDao {
         return requests;
     }
 
+    public List<Request> getByUserId(long userId) {
+        session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Query query = session.createQuery("from Request where user_id =" + userId);
+        return ((org.hibernate.query.Query) query).list();
+    }
 
-    //get by user id
+    public List<Request> getByDuckId(long duckId) {
+        session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Query query = session.createQuery("from Request where duck_id =" + duckId);
+        return ((org.hibernate.query.Query) query).list();
+    }
 
-    //TODO: get by user id, get by duck id, get by accessibility
+    public List<Request> getByIsApproved(boolean isApproved) {
+        session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Query query = session.createQuery("from Request where is_approved =" + isApproved);
+        return ((org.hibernate.query.Query) query).list();
+    }
+
+    private List<Request> handleJoinResult(Query query) {
+        List<Request> requests = new ArrayList<>();
+        List<Object[]> objects = ((org.hibernate.query.Query) query).list();
+        Iterator iterator = objects.iterator();
+        while (iterator.hasNext()) {
+            Object[] obj = (Object[]) iterator.next();
+            Request request = (Request) obj[0];
+            requests.add(request);
+        }
+        session.close();
+        return requests;
+    }
+
 }
--- src/main/java/se/ifmo/ru/service/RequestService.java
+++ src/main/java/se/ifmo/ru/service/RequestService.java
@@ -13,7 +13,11 @@ public class RequestService {
     public Request getById(long id) {
         return requestDao.getById(id);
     }
-    
+
+    public Request getByIdWithUserAndDuck(long id) {
+        return requestDao.getByIdWithUserAndDuck(id);
+    }
+
     public void save(Request request) {
         requestDao.save(request);
     }
@@ -28,5 +32,17 @@ public class RequestService {
 
     public List<Request> getAll() {
         return requestDao.getAll();
+    }
+
+    public List<Request> getByUserId(long userId) {
+        return requestDao.getByUserId(userId);
+    }
+
+    public List<Request> getByDuckId(long duckId) {
+        return requestDao.getByDuckId(duckId);
+    }
+
+    public List<Request> getByIsApproved(boolean isApproved) {
+        return requestDao.getByIsApproved(isApproved);
     }
 }
